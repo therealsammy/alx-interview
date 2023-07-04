e5519e301adbbf94d90a6d4577a3d431de700894
Samuel Ohiri <therealsammy18x@gmail.com>
2023-07-04 09:13:06 +0100

    File updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,21 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Defines a function that determines if a box containing a list
    of lists can be opened using keys stored in the lists
 """
 
+
 def canUnlockAll(boxes):
-    """
-    Checks if all the boxes can be opened.
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
 
-    Args:
-        boxes (List[List[int]]): A list of lists, where each inner list represents a box and contains integers that are keys to other boxes.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    if not boxes or len(boxes) == 0:
-        return False
-
-    n = len(boxes)
-    visited = set()
-    queue = [0]  # Starting with box 0
-
-    while queue:
-        current_box = queue.pop(0)
-        visited.add(current_box)
-
-        for key in boxes[current_box]:
-            if key not in visited:
-                queue.append(key)
-
-    return len(visited) == n
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
